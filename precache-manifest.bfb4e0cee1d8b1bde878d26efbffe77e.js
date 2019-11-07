8df1d8bfd3a9909f5e843b0399e3d0559e52fbed
Daniel Wong <danielwong@dropbox.com>
2019-11-06 16:13:07 -0800

    Updates

---
+++ precache-manifest.bfb4e0cee1d8b1bde878d26efbffe77e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15da44aad730f655350e90bb5ea727b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "61dcc8bd7a570ffbeb76",
+    "url": "/monsters-rolodex/static/css/main.766d2202.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "61dcc8bd7a570ffbeb76",
+    "url": "/monsters-rolodex/static/js/main.c431194b.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
